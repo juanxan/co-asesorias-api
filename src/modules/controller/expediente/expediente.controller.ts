9f3238eec868a165cbeab2e2fcd6e3ca79c3f251
juanxan <juanxan123@gmail.com>
2024-10-26 09:15:49 -0500

    protected expediente controller

--- src/modules/controller/expediente/expediente.controller.ts
+++ src/modules/controller/expediente/expediente.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { Observable } from 'rxjs';
 import { ExpedienteResponseDto } from './../../models/expediente-response.model';
 import { ExpedienteService } from './../../service/expediente/expediente.service';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('local'))
 @Controller('expedientes')
 @ApiTags('expedientes')
 export class ExpedienteController {
-	constructor(private readonly expedienteService: ExpedienteService) {}
+  constructor(private readonly expedienteService: ExpedienteService) {}
 
-	@Get()
-	@ApiOperation({ summary: 'Get information about expedient by user' })
-	getExpedientes(): Observable<ExpedienteResponseDto[]> {
-		return this.expedienteService.getExpedientes();
-	}
+  @Get()
+  @ApiOperation({ summary: 'Get information about expedient by user' })
+  getExpedientes(): Observable<ExpedienteResponseDto[]> {
+    return this.expedienteService.getExpedientes();
+  }
 }
