bdb3e4f678b751e8ab4c29e17e706f70d7a0099c
juanxan <juanxan123@gmail.com>
2024-10-26 21:53:53 -0500

    add countries controller and service in module

--- src/modules/parkit-module.module.ts
+++ src/modules/parkit-module.module.ts
@@ -8,20 +8,23 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { ExpedienteController } from './controller/expediente/expediente.controller';
 import { SingInController } from './controller/sing-in/sing-in.controller';
 import { SingUpController } from './controller/sing-up/sing-up.controller';
+import { CountriesController } from './controller/countries/countries.controller';
 
 import { ExpedienteService } from './service/expediente/expediente.service';
 import { SingInService } from './service/sing-in/sing-in.service';
 import { SingUpService } from './service/sing-up/sing-up.service';
+import { CountriesService } from './service/countries/countries.service';
 
 import { DatabaseModule } from './database/database.module';
 import { JwtStrategy } from './strategie/jwt.strategy';
 import { LocalStrategy } from './strategie/local.strategy';
 import { Users } from './database/schema.db';
+import { Countries } from './database/countries.db';
 import config from './../config';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Users]),
+    SequelizeModule.forFeature([Users, Countries]),
     DatabaseModule,
     PassportModule,
     HttpModule,
@@ -34,11 +37,17 @@ import config from './../config';
       },
     }),
   ],
-  controllers: [SingInController, SingUpController, ExpedienteController],
+  controllers: [
+    SingInController,
+    SingUpController,
+    ExpedienteController,
+    CountriesController,
+  ],
   providers: [
     SingInService,
     SingUpService,
     ExpedienteService,
+    CountriesService,
     LocalStrategy,
     JwtStrategy,
   ],
